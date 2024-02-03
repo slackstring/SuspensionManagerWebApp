302b803e918eb0c0450e45b811107962db77f744
slackstring <slackstring88@gmail.com>
2024-02-03 16:53:59 +0100

    added tableToIndexView/added Delete function

--- SuspensionManagerWebApp/Data/ApplicationDbContext.cs
+++ SuspensionManagerWebApp/Data/ApplicationDbContext.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuspensionManagerWebApp.Models;
 
 namespace SuspensionManagerWebApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<SusElement> SusElements { get; set; }
+        public DbSet<AirForkSetting> AirForkSettings { get; set; }
+        public DbSet<AirShockSetting> AirShockSettings { get; set; }
+        public DbSet<CoilShockSetting> CoilShockSettings { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
