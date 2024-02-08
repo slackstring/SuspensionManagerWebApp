e6d2eacec9b03fb3a92c675e1fb05fd9ae46710e
slackstring <slackstring88@gmail.com>
2024-02-08 20:32:32 +0100

    changeOfModels Setting/SusElement/InheritedClasses

--- SuspensionManagerWebApp/Data/ApplicationDbContext.cs
+++ SuspensionManagerWebApp/Data/ApplicationDbContext.cs
@@ -7,9 +7,6 @@ namespace SuspensionManagerWebApp.Data
     public class ApplicationDbContext : IdentityDbContext
     {
         public DbSet<SusElement> SusElements { get; set; }
-        public DbSet<AirForkSetting> AirForkSettings { get; set; }
-        public DbSet<AirShockSetting> AirShockSettings { get; set; }
-        public DbSet<CoilShockSetting> CoilShockSettings { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
